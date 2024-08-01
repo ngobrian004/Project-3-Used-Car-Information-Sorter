6b1cd66364336aa9aca856cab259087456e57fd5
ngobrian004 <ngobrian@ufl.edu>
2024-08-01 10:11:25 -0400

    Update main.py
    Added comments and reformatted some code to make it more consistent

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ year_arr = df["year"].to_numpy()
 manufacturer_arr = df["manufacturer"].to_numpy()
 condition_arr = df["condition"].to_numpy()
 
-data = [price_arr, year_arr, manufacturer_arr, condition_arr]
-
 
 def merge(arr, left, mid, right):
     # Create X ← arr[left..mid] & Y ← arr[mid+1..right]
@@ -60,27 +58,20 @@ def merge_sort(arr, left, right):
         merge(arr, left, mid, right)
 
 
-def heap_sort(arr, n):
-    for i in range((n // 2) - 1, -1, -1):
-        heapify(arr, n, i)
-
-    for i in range(n - 1, 0, -1):
-        temp = arr[0]
-        arr[0] = arr[i]
-        arr[i] = temp
-        heapify(arr, i, 0)
-
-
 def heapify(arr, n, i):
+    # Builds a max heap
     larger = i
+    # These are the positions of the left and right children
     left = (2 * i) + 1
     right = (2 * i) + 2
 
+    # Mark the largest child if it is bigger than parent
     if left < n and arr[left] > arr[larger]:
         larger = left
     if right < n and arr[right] > arr[larger]:
         larger = right
 
+    # Swap if there is a larger child
     if larger != i:
         temp = arr[larger]
         arr[larger] = arr[i]
@@ -89,9 +80,24 @@ def heapify(arr, n, i):
         heapify(arr, n, i)
 
 
+def heap_sort(arr, n):
+    for i in range((n // 2) - 1, -1, -1):
+        # Builds a heap in place
+        heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        # Extracts elements one by one
+        temp = arr[0]
+        arr[0] = arr[i]
+        arr[i] = temp
+        heapify(arr, i, 0)
+
+
 def year(arr):
+    # Creates 2 copies of the input array
     heap_copy = arr.copy()
     merge_copy = arr.copy()
+    # Sorts with heap sort and measures time taken
     start = time.time()
     heap_sort(heap_copy, len(heap_copy))
     end = time.time()
@@ -99,15 +105,18 @@ def year(arr):
     print("\nHeap sort:", length, "seconds")
 
     print("Number of cars for each year (Heap Sort):")
+    # Create and fill out a dict using sorted array
     year_count_heap = {}
     for x in heap_copy:
         if x in year_count_heap:
             year_count_heap[x] += 1
         else:
             year_count_heap[x] = 1
-    for year in year_count_heap.keys():
-        print(f"{year}: {year_count_heap[year]} cars")
+    # Print the dict
+    for x in year_count_heap.keys():
+        print(f"{x}: {year_count_heap[x]} cars")
 
+    # Repeat for merge sort
     start = time.time()
     merge_sort(merge_copy, 0, len(merge_copy) - 1)
     end = time.time()
@@ -121,9 +130,10 @@ def year(arr):
             year_count_merge[x] += 1
         else:
             year_count_merge[x] = 1
-    for year in year_count_merge.keys():
-        print(f"{year}: {year_count_merge[year]} cars")
+    for x in year_count_merge.keys():
+        print(f"{x}: {year_count_merge[x]} cars")
 
+    # Checks to see if the sorts produced the same result
     if (merge_copy == heap_copy).all():
         print("\nSuccess\n")
     else:
@@ -131,6 +141,7 @@ def year(arr):
 
 
 def price(arr):
+    # All functions work similarly to the first with minor changes to printing method
     heap_copy = arr.copy()
     merge_copy = arr.copy()
 
@@ -150,11 +161,6 @@ def price(arr):
             price_count_heap[category] = 1
     for category in price_count_heap.keys():
         print(f"${category} - ${category + 9999}: {price_count_heap[category]} cars")
-
-    # prints out every price for heap copy (used for testing)
-    # print("\nPrices after Heap Sort:")
-    # for prices in heap_copy:
-    # print(prices)
 
     start = time.time()
     merge_sort(merge_copy, 0, len(merge_copy) - 1)
@@ -205,7 +211,7 @@ def manufacturer(arr):
     length = end - start
     print("\nMerge sort:", length, "seconds")
 
-    print("\nNumber of cars for each manufacturer  (Merge Sort):")
+    print("\nNumber of cars for each manufacturer (Merge Sort):")
     manufacturer_count_merge = {}
     for x in merge_copy:
         if x in manufacturer_count_merge:
@@ -224,7 +230,6 @@ def manufacturer(arr):
 def condition(arr):
     heap_copy = arr.copy()
     merge_copy = arr.copy()
-    dict_order = ['new', 'like new', 'excellent', 'good', 'fair', 'salvage']
 
     start = time.time()
     heap_sort(heap_copy, len(heap_copy))
@@ -239,8 +244,7 @@ def condition(arr):
             condition_count_heap[x] += 1
         else:
             condition_count_heap[x] = 1
-    res = {key: condition_count_heap[key] for key in dict_order}
-    for category in res.keys():
+    for category in condition_count_heap.keys():
         print(f"{category.title()}: {condition_count_heap[category]} cars")
 
     start = time.time()
@@ -256,8 +260,7 @@ def condition(arr):
             condition_count_merge[x] += 1
         else:
             condition_count_merge[x] = 1
-    res = {key: condition_count_heap[key] for key in dict_order}
-    for category in res.keys():
+    for category in condition_count_heap.keys():
         print(f"{category.title()}: {condition_count_merge[category]} cars")
 
     if (merge_copy == heap_copy).all():
